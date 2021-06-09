dca5a0d54366eef70ea19cf7b323ea5e65dceb39
Isaac <izaroot327@gmail.com>
2021-06-09 16:51:31 -0400

    Completed Lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+    def clickbait?
+        # clickbaity = ["Won't Believe", "Secret", "Guess", "Top" ]
+        if title && !title.include?("Won't Believe")
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
+    
+#   CLICKBAIT_PATTERNS = [
+#     /Won't Believe/i,
+#     /Secret/i,
+#     /Top \d/i,
+#     /Guess/i
+#   ]
+
+#   def clickbait?
+#     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+#       errors.add(:title, "must be clickbait")
+#     end
+#   end
+
 end
